cd9123ed0783fbca5a2e5a384b38388ec5f09490
OEmiliatanO <emiliaistruelove@gmail.com>
2021-11-16 00:26:55 -0800

    all done

--- src/complex.h
+++ src/complex.h
@@ -7,8 +7,6 @@ using std::ostream;
 
 class Complex
 {
-private:
-    double re, im;
 public:
     Complex(const double re = 0, const double im = 0);
     Complex(const Complex& c);
@@ -31,7 +29,6 @@ public:
     ~Complex() = default;
 
     friend Complex polar(const double leng, const double arg);
-    
     friend double norm(const Complex& x);
     friend double abs(const Complex& x);
     friend double arg(const Complex& x);
@@ -46,6 +43,11 @@ public:
     friend bool operator==(const Complex& x, const Complex& y);
     friend bool operator!=(const Complex& x, const Complex& y);
     friend ostream& operator<<(ostream& o, const Complex& x);
+    
+private:
+    double re, im;
 };
+
+Complex polar(const double leng, const double arg);
 
 #endif
